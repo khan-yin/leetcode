2e63079c3b989ceaad7d2dc4648b390390e98761
khannb <838278270@qq.com>
2020-09-16 19:06:39 +0800

    day10

---
+++ src/环路监测/Solution.java
@@ -0,0 +1,32 @@
+package 环路监测;
+
+import 移除重复节点.ListNode;
+
+public class Solution {
+    //双指针牛逼
+    public ListNode detectCycle(ListNode head) {
+        if(head==null||head.next==null){
+            return null;
+        }
+        ListNode low=head;
+        ListNode fast=head;
+        ListNode p;
+        while(low!=null&&fast!=null){
+            low=low.next;
+            fast=fast.next.next;
+            if(fast==null||fast.next==null){
+                return null;
+            }else{
+                if(fast==low){
+                    break;
+                }
+            }
+        }
+        fast=head;
+        while(fast!=low){
+            fast=fast.next;
+            low=low.next;
+        }
+        return fast;
+    }
+}
