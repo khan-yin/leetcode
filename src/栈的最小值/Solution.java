7ebec1e2b97dbcb983207cdf2c7ef3ae2a48a502
khannb <838278270@qq.com>
2020-09-18 10:53:45 +0800

    day11

---
+++ src/栈的最小值/Solution.java
@@ -0,0 +1,41 @@
+package 栈的最小值;
+
+import java.util.Stack;
+
+public class Solution {
+    private Stack<Integer> stack;
+
+    private Stack<Integer> minstack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minstack = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if(minstack.size() <= 0) {
+            minstack.push(x);
+        }else {
+            if(minstack.peek() < x) {
+                minstack.push(minstack.peek());
+            }else {
+                minstack.push(x);
+            }
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minstack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minstack.peek();
+    }
+}
