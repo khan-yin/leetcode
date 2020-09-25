ef9fbd2c29bceef90d5eb791ce4c812f3fd32780
khannb <838278270@qq.com>
2020-09-25 13:12:53 +0800

    day16

--- src/第四章/后继者/Solution.java
+++ src/第四章/后继者/Solution.java
@@ -5,7 +5,7 @@ import 第四章.最小高度树.TreeNode;
 import java.util.Stack;
 
 public class Solution {
-    //中序遍历法
+    //中序遍历法，二叉搜索树采用中序遍历法，得到的就是整个有序数组序列
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode m = root;
