c438f6a827b88b4f0f52d67452b6700ff0431020
khannb <838278270@qq.com>
2020-09-26 22:27:46 +0800

    day17

---
+++ src/第四章/首个共同祖先/Solution.java
@@ -0,0 +1,20 @@
+package 第四章.首个共同祖先;
+
+import 第四章.最小高度树.TreeNode;
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p == root || q == root) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
