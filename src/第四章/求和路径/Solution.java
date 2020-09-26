4012097a22dac6e5f046552b6cf7bd3344fe8164
khannb <838278270@qq.com>
2020-09-26 23:46:45 +0800

    day17

---
+++ src/第四章/求和路径/Solution.java
@@ -0,0 +1,48 @@
+package 第四章.求和路径;
+
+import 第四章.最小高度树.TreeNode;
+
+public class Solution {
+    // public int total=0;
+    public int cnt = 0;
+    public int sum = 0;
+
+    public void dfs(TreeNode root, int total) {
+
+        if (root == null) {
+            System.out.println("x");
+            return;
+        } else {
+            total += root.val;
+        }
+        System.out.println(total);
+        if (total == sum) {
+            cnt++;
+            System.out.println("cnt=" + cnt);
+        }
+        dfs(root.left, total);
+        dfs(root.right, total);
+    }
+
+    public void qianxu(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        int total = 0;
+        dfs(root, total);
+        if (root.left != null) {
+            qianxu(root.left);
+            // System.out.println(root.left.val);
+        }
+        if (root.right != null) {
+            qianxu(root.right);
+        }
+
+    }
+
+    public int pathSum(TreeNode root, int sum) {
+        this.sum = sum;
+        qianxu(root);
+        return this.cnt;
+    }
+}
