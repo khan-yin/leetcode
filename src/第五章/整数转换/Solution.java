4f14b19ec7a5bec691279d540bb97a603087a4de
khannb <838278270@qq.com>
2020-09-30 11:58:53 +0800

    day21

---
+++ src/第五章/整数转换/Solution.java
@@ -0,0 +1,28 @@
+package 第五章.整数转换;
+
+public class Solution {
+    public int convertInteger(int A, int B) {
+        int k = 0;
+        int c = A ^ B;
+        String s3 = Integer.toBinaryString(c);
+        char[] ch = s3.toCharArray();
+        for (char cm : ch) {
+            if (cm == '1') {
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public int convertInteger2(int A, int B) {
+        int k = 0;
+        int c = A ^ B;
+        int c = A ^ B;
+        while (c != 0) {
+            k++;
+            c &= (c - 1);
+        }
+        return k;
+    }
+
+}
