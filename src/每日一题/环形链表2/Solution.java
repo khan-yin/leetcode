35cc38eefae02942c7fce606318ba651b3bdd8fd
khannb <838278270@qq.com>
2020-10-10 19:13:45 +0800

    day28

--- src/每日一题/环形链表2/Solution.java
+++ src/每日一题/环形链表2/Solution.java
@@ -6,6 +6,35 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
+    //快慢指针法
+    public ListNode detectCycle2(ListNode head) {
+        ListNode fast = head;
+        ListNode low = head;
+        ListNode together = null;
+        while (fast != null && low != null && fast.next != null) {
+            fast = fast.next.next;
+            low = low.next;
+            if (fast == low) {
+                together = fast;
+                break;
+            }
+        }
+        if (together == null)
+            return together;
+        else {
+            // System.out.println(together.val);
+            ListNode p = head;
+            ListNode q = together;
+            while (p != null && q != null) {
+                if (p == q)
+                    return p;
+                p = p.next;
+                q = q.next;
+            }
+        }
+        return null;
+    }
+
     //哈希集合法
     public ListNode detectCycle(ListNode head) {
         if (head == null)
