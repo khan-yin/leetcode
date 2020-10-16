bc5883ba2b07ca1a441055e6b49a00dae7d97756
khannb <838278270@qq.com>
2020-10-16 21:20:45 +0800

    day32

--- src/第八章/硬币/Solution.java
+++ src/第八章/硬币/Solution.java
@@ -1,4 +1,18 @@
 package 第八章.硬币;
 
 public class Solution {
+    static final int MOD = 1000000007;
+    int[] coins = {25, 10, 5, 1};
+
+    public int waysToChange(int n) {
+        int[] waylist = new int[n + 1];
+        waylist[0] = 1;
+        for (int i = 0; i < 4; i++) {
+            int coin = coins[i];
+            for (int j = coin; j <= n; j++) {
+                waylist[j] = (waylist[j] + waylist[j - coin]) % MOD;
+            }
+        }
+        return waylist[n];
+    }
 }
