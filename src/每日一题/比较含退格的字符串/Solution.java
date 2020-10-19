2f62de7a9ab82365d681c6e8c6f8fe611b975d7d
khannb <838278270@qq.com>
2020-10-20 00:27:10 +0800

    day35

--- src/每日一题/比较含退格的字符串/Solution.java
+++ src/每日一题/比较含退格的字符串/Solution.java
@@ -1,5 +1,34 @@
 package 每日一题.比较含退格的字符串;
 
-public class Solution {
+import java.util.Stack;
 
+public class Solution {
+    public static boolean backspaceCompare(String S, String T) {
+        Stack<Character> s = new Stack<>();
+        Stack<Character> t = new Stack<>();
+        for (char a : S.toCharArray()) {
+            if (a == '#') {
+                if (!s.isEmpty())
+                    s.pop();
+                continue;
+            }
+            s.push(a);
+        }
+        for (char b : T.toCharArray()) {
+            if (b == '#') {
+                if (!t.isEmpty())
+                    t.pop();
+                continue;
+            }
+            t.push(b);
+        }
+        if (s.size() != t.size()) {
+            return false;
+        }
+        return s.equals(t);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(backspaceCompare("y#fo##f", "y#f#o##f"));
+    }
 }
