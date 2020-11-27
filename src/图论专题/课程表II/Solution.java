4c626c251bd5d7b3c281491b443a1355d7fefc38
khannb <838278270@qq.com>
2020-11-27 13:10:21 +0800

    day52

--- src/图论专题/课程表II/Solution.java
+++ src/图论专题/课程表II/Solution.java
@@ -6,6 +6,43 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Solution {
+    //拓扑排序经典题目，bfs操作更好
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, ArrayList<Integer>> nodemaplist = new HashMap<>();
+        if (numCourses <= 1)
+            return true;
+        int[] enter = new int[numCourses];//入度
+        int[] to = new int[numCourses];//出度
+        for (int[] courserelation : prerequisites) {
+            int target = courserelation[0];
+            int pre = courserelation[1];
+            enter[target]++;
+            to[pre]++;
+            if (!nodemaplist.containsKey(pre))//对先驱节点创建邻接表
+                nodemaplist.put(pre, new ArrayList<Integer>());
+            nodemaplist.get(pre).add(target);
+        }
+        Queue<Integer> queue = new LinkedList<>();//用队列来维护bfs遍历
+        int cnt = 0;//记录访问的节点数
+        for (int i = 0; i < enter.length; i++) {
+            if (enter[i] == 0)
+                queue.offer(i);//初始化队列，将queue加入其中
+        }
+
+        while (!queue.isEmpty()) {
+            int temp = queue.remove();
+            cnt++;
+            if (to[temp] == 0)//出度为0则无需遍历
+                continue;
+            for (int target : nodemaplist.get(temp)) {
+                enter[target]--;
+                if (enter[target] == 0)
+                    queue.offer(target);
+            }
+        }
+        return cnt == numCourses;
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         HashMap<Integer, ArrayList<Integer>> nodemaplist = new HashMap<>();
         int[] res = new int[numCourses];
