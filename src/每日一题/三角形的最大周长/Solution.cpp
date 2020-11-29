536167aa72748b57e6f9aa2f8334d65d012be3b5
khannb <838278270@qq.com>
2020-11-29 15:43:30 +0800

    day53

---
+++ src/每日一题/三角形的最大周长/Solution.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& A) {
+        sort(A.begin(),A.end());
+        for(int i=A.size()-1;i>=2;i--)
+        {
+            if(A[i-1]+A[i-2]>A[i])
+                return A[i-1]+A[i-2]+A[i];
+        }
+        return 0;
+    }
+};
