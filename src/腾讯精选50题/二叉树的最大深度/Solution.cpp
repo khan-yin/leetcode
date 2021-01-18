32d33e3d512569bddc4d308a69fadd0af8dac90e
khannb <838278270@qq.com>
2021-01-18 20:48:50 +0800

    day75

---
+++ src/腾讯精选50题/二叉树的最大深度/Solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public: 
+    int maxDepth(TreeNode* root) {
+        if(root!=nullptr)
+        {
+            int left=maxDepth(root->left);
+            int right=maxDepth(root->right);
+            return max(left,right)+1;
+        }
+        return 0;
+    }
+};
