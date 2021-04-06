fda6576be97a58601a1429d4642b98123f560f0f
khannb <838278270@qq.com>
2021-04-06 13:46:07 +0800

    day106

---
+++ src/堆专题/数据流中的第 K 大元素/solution.cpp
@@ -0,0 +1,24 @@
+#include<queue>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> q;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (int x: nums) {
+            add(x);
+        }
+    }
+    
+    int add(int val) {
+        q.push(val);
+        if (q.size() > k) {
+            q.pop();
+        }
+        return q.top();
+    }
+};
