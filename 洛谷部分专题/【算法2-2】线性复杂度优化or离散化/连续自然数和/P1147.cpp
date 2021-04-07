1a0dce5e0f60aa8032a231857eeaa002c2063fe8
khannb <838278270@qq.com>
2021-04-07 16:32:37 +0800

    day107

--- 洛谷部分专题/【算法2-2】线性复杂度优化or离散化/连续自然数和/P1147.cpp
+++ 洛谷部分专题/【算法2-2】线性复杂度优化or离散化/连续自然数和/P1147.cpp
@@ -2,16 +2,20 @@
 #include<cstdio>
 using namespace std;
 long long n,a,b,c;
-int main() {
+int main() 
+{
 	scanf("%d",&n);
-	for(a=1; a<=n-1; a++) {
-		for(b=a+1; b<=n; b++) {
+	for(a=1; a<=n-1; a++) 
+	{
+		for(b=a+1; b<=n; b++) 
+		{
 			c=(b-a)+1;
-			if(a*c>=n) break;//剪枝
-			if(((a+b)*c/2)==n) {
+			if(a*c>=n) 
+				break;//剪枝
+			if(((a+b)*c/2)==n) 
+			{
 				printf("%d ",a);
 				printf("%d\n",b);
-				break;
 			}
 		}
 	}
