986c511fb4ad2fa64a3c00a1226ecf944dade207
khannb <838278270@qq.com>
2021-04-18 15:19:25 +0800

    day117

---
+++ src/每日一题/删除有序数组中的重复项/solution.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+};
