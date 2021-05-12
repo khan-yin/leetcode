93fa42465b44186f0390eb1b0481697404990f92
khannb <838278270@qq.com>
2021-05-12 21:50:19 +0800

    day132

---
+++ acwing/动态规划/背包问题/AcWing3完全背包问题朴素版.cpp
@@ -0,0 +1,95 @@
+// 原理版
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1010;
+
+int v[N], w[N];
+
+int dp[N][N];
+
+
+int main()
+{
+    int n,V;
+    
+    scanf("%d%d", &n, &V);
+    for (int i = 1; i <= n; i ++ )
+        scanf("%d%d", &v[i], &w[i]);
+    
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=V;j++)
+            for(int k=0;v[i]*k<=j;k++)
+                dp[i][j] = max(dp[i][j],dp[i-1][j - v[i]*k]+w[i]*k);
+    
+    cout<<dp[n][V];    
+    return 0;
+}
+
+// 朴素版
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1010;
+
+int v[N], w[N];
+
+int dp[N][N];
+
+
+int main()
+{
+    int n,V;
+    
+    scanf("%d%d", &n, &V);
+    for (int i = 1; i <= n; i ++ )
+        scanf("%d%d", &v[i], &w[i]);
+    
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=V;j++)
+        {
+            dp[i][j] = dp[i-1][j];
+            if(j>=v[i])
+                dp[i][j] = max(dp[i][j],dp[i][j-v[i]]+w[i]);
+        }
+    
+    cout<<dp[n][V];    
+    return 0;
+}
+
+// 一维优化版
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1010;
+
+int v[N], w[N];
+
+int DP[N];
+
+
+int main()
+{
+    int n,V;
+    
+    scanf("%d%d", &n, &V);
+    for (int i = 1; i <= n; i ++ )
+        scanf("%d%d", &v[i], &w[i]);
+    
+    for(int i=1;i<=n;i++)
+        for(int j=v[i];j<=V;j++)
+            DP[j] = max(DP[j],DP[j-v[i]]+w[i]);
+    
+    cout<<DP[V];    
+    return 0;
+}
