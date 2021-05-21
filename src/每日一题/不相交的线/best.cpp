286a003b5821ff376ab20994d34d36772476de36
khannb <838278270@qq.com>
2021-05-21 16:21:40 +0800

    day137

--- src/每日一题/不相交的线/best.cpp
+++ src/每日一题/不相交的线/best.cpp
@@ -10,11 +10,10 @@ int dp[510][510];//表示a[]前i和b[]前j个位置的最大连线数
 class Solution {
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size(),m=nums2.size();
-        for(int i=0;i<n;i++)
-            a[i+1] = nums1[i];
-        for(int i=0;i<m;i++)
-            b[i+1] = nums2[i]; 
+        int n = nums1.size(), m = nums2.size();
+        for(int i=0;i<n;i++) a[i+1] = nums1[i];
+        for(int i=0;i<m;i++) b[i+1] = nums2[i]; 
+
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
