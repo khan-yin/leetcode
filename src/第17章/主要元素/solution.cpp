e2009aaaf8ff66578292089a30c8f2ee03aecd3e
khannb <838278270@qq.com>
2021-05-26 00:14:09 +0800

    day140

---
+++ src/第17章/主要元素/solution.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> m;
+        for(auto i : nums)
+        {
+            m[i]++;
+            if(m[i]>n/2) return i;
+        }
+        return -1;
+    }
+};
