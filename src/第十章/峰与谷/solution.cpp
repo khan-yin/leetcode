414be6c453d5c35d21a2707b8a6f26706f9901d1
khannb <838278270@qq.com>
2021-05-28 20:06:41 +0800

    day143

---
+++ src/第十章/峰与谷/solution.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        for(int i=1;i<nums.size();i++){
+            if(i&1){
+                if(nums[i]>nums[i-1]) swap(nums[i],nums[i-1]);
+            }
+            else{
+                if(nums[i]<nums[i-1]) swap(nums[i],nums[i-1]);
+            }
+        }
+    }
+};
